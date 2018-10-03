3833acda17a650a1f531d68874d09b6ec86b0e3e
rscheiwe <rscheiwe@gmail.com>
2018-10-03 12:00:49 -0400

    added euler work

---
+++ euler/Euler1.js
@@ -0,0 +1,16 @@
+//number 1
+function range(start, count) {
+  return Array.apply(0, Array(count))
+    .map((element, index) => index + start)
+}
+
+function multiples(number) {
+  let store = []
+  for (let x of range(1,number)) {
+    if (x % 3 === 0 || x % 5 === 0) {
+      store.push(x)
+    }
+
+  }
+  return store.reduce((a,b) => a+b)
+}
