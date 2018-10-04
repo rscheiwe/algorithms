68a95f729a21573b1491bb2484c81e79a1efd61e
rscheiwe <rscheiwe@gmail.com>
2018-10-04 11:03:50 -0400

    added test3

--- euler/eulers/euler3.js
+++ euler/eulers/euler3.js
@@ -13,8 +13,48 @@ Divide the number starting with 3 till the square root of the input number.
 At the end of the for loop if the number is greater than 2 then add the number to the primeFactors array.
 */
 
-function primeFactor(n) {
-  let factors = []
+var getPrimeFactors = function(number) {
+    var i = 2;
+    var limit = Math.floor(number/2);
+    var factors = [];
+    while (i < limit) {
+        // Check if number is divisible by i, if yes add i to list of factors and repeat
+        // (There can be multiple prime factors of the same value, e.g. 12=2*2*3)
+        while (number % i === 0) {
+            number /= i;
+            factors.push(i);
 
-  
+        }
+        i++;
+        // Lower the limit so the loop does only run as long as necessary
+        limit = Math.floor(number/2);
+    }
+    // After going through all the lower numbers, the remaining on must be (?) prime
+    factors.push(number);
+    return Math.max.apply(null,factors);
 }
+
+module.exports = getPrimeFactors
+//
+// function primeFactor(n) {
+//   let factors = []
+//   while (n % 2 === 0) {
+//     factors.push(2)
+//     n = n/2
+//   }
+//
+//   let sqrtNum = Math.sqrt(n);
+//   for (let i = 3; i <= sqrtNum; i++) {
+//     while (n % i === 0) {
+//       factors.push(i)
+//       i++
+//     }
+//   }
+//
+//   if (n > 2) {
+//     factors.push(n)
+//   }
+//
+//   return factors
+//
+// }
--- euler/test/euler3.test.js
+++ euler/test/euler3.test.js
@@ -1,10 +1,10 @@
 const assert = require('assert')
-const largestPrime = require('../eulers/euler3.js')
+const getPrimeFactors = require('../eulers/euler3.js')
 
 describe('Test3', () => {
   it('find the largest prime number for a given value', () => {
 
-    assert.equal(largestPrime(10000), 4613732)
+    assert.equal(getPrimeFactors(13195), 29)
   })
 
 })
