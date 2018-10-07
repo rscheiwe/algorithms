e85fb549dd2c5f5c8ec3ae393b0125d6e7bf39c7
rscheiwe <rscheiwe@gmail.com>
2018-10-07 18:55:50 -0400

    updated chars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+//also frequently used for Anagram questions
+
+function maxChar(str) {
+  let letters = {}
+  for (let char of str) {
+    //letters[char] = letters[char] + 1 || 1
+    letters[char] ? letters[char] += 1 : letters[char] = 1
+  }
+  return Object.keys(letters).reduce((a,b) => letters[a] > letters[b] ? a : b)
+}
 
 module.exports = maxChar;
