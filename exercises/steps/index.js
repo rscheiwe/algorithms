c1925bdae748e61ecd5078e966361b93214e630b
rscheiwe <rscheiwe@gmail.com>
2018-10-19 08:36:38 -0400

    recursive nth solution steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,25 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {
+function steps(n, row = 0, stair = '') {
 
-  for (let row = 0; row < n; row++) {
-    let stair = ''
-
-    for (let col = 0; col < n; col++) {
-      if (col <= row) {
-        stair += '#'
-      } else {
-        stair += ' '
-      }
-    }
-    console.log(stair)
+  if (n === row) {
+    return
   }
-}
+
+  if (n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row) {
+    stair += '#'
+  } else {
+    stair += ' '
+  }
+  steps(n, row, stair)
+  // for (let row = 0; row < n; row++) {
+  //   let stair = ''
+  //
+  //   for (let col = 0; col < n; col++) {
+  //     col <= row ? stair += '#' : stair += ' '
+  //     }
+  //   console.log(stair)
+  // }
+
 
 //   for (let i = 1; i <= n; i++) {
 //     console.log(('#'.repeat(i)) + (' '.repeat(n-i)))
 //   }
 // }
+
+}
 
 module.exports = steps;
