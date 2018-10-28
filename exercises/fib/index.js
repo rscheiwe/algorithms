ce3c5bbac459a989972de72525baf20960586ead
rscheiwe <rscheiwe@gmail.com>
2018-10-28 18:25:05 -0400

    recursive fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,13 +9,18 @@
 //   fib(4) === 3
 
 function fib(n) {
-  const result = [0,1]
-  for (let i = 2; i <= n; i++) {
-    const a = result[i - 1]
-    const b = result[i - 2]
-    result.push(a + b)
-  }
-  return result[n]
+
+  if (n < 2) return n
+
+  return fib(n-1) + fib(n-2)
+
+  // const result = [0,1]
+  // for (let i = 2; i <= n; i++) {
+  //   const a = result[i - 1]
+  //   const b = result[i - 2]
+  //   result.push(a + b)
+  // }
+  // return result[n]
 }
 
 module.exports = fib;
