eca6a7b44010117133c976a125706201d2b08bb0
rscheiwe <rscheiwe@gmail.com>
2018-10-28 20:55:50 -0400

    refactored for memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,19 +8,36 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {}
+  return function(...args) {
+    //have we called this function with this particular
+    // set of args before? If so, return that.
+    if (cache[args]) {
+      return cache[args]
+    }
+    const result = fn.apply(this, args)
+    cache[args] = result
+    return result
+  }
+}
 
+function fib(n) {
+  //linear time solution
+  // const result = [0,1]
+  // for (let i = 2; i <= n; i++) result.push(result[i - 1] + result[i - 2])
+  //
+  // return result[n]
+
+  //exponential time solution
+  // => recursive function (fib(n)) being called multiple times with identical args
   if (n < 2) return n
 
   return fib(n-1) + fib(n-2)
 
-  // const result = [0,1]
-  // for (let i = 2; i <= n; i++) {
-  //   const a = result[i - 1]
-  //   const b = result[i - 2]
-  //   result.push(a + b)
-  // }
-  // return result[n]
 }
+
+fib = memoize(fib)
+
 
 module.exports = fib;
